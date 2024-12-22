4dd18f410cf7b75031c683de6b2075dbec45d349
prajnak1 <126357555+prajnak1@users.noreply.github.com>
2024-12-22 15:41:10 -0500

    Update page.tsx

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,5 +1,6 @@
 //page: addressable route to server
 //used params inside of jsx to render the jsx component
+//page transfers without a full reload of the browser (client side routing)
 export default function Page({params}:{
     params: {id: string}
 }) {
